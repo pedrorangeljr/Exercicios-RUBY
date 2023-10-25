3778f679211978b68906b4be10bd6d7f4ba64acd
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-10-25 08:37:50 -0300

    exe28

---
+++ expressoesVariaveis/exe28.rb
@@ -0,0 +1,10 @@
+puts "";
+n1 = Integer(gets());
+puts "";
+n2 = Integer(gets());
+puts "";
+n3 = Integer(gets());
+
+soma = (n1.pow(2)) + (n2.pow(2)) + (n3.pow(2));
+
+puts "A soma dos quadrados: #{soma}";

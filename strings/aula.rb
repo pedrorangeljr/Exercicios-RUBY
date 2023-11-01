84913c6963b82ee27244e96f55d964fc2143350c
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-11-01 11:21:49 -0300

    strings aula2

--- strings/aula.rb
+++ strings/aula.rb
@@ -3,4 +3,20 @@ a ="nossa aula de hoje "
 
 #a.gsub!("aula", "aula 2")
 
-puts a[0,5]
+# puts a[0,5]
+
+# puts a.split(" ")
+
+# puts a.upcase Tudo em maiusculo
+
+# puts a.downcase -- tudo em minusculo
+
+# puts a.capitalize -- so a promeira é maiuscula
+
+# puts a.delete("aula") -- deleta uma parte da estring especificada
+
+# puts a.strip
+
+# puts a.index("nossa") --mostra a posição da string especificada
+
+puts a.reverse

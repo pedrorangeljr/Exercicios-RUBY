eb92f5eaf530849c65296b61949a6bfda63d1eaf
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-11-14 12:03:40 -0300

    exe3.1

---
+++ array-hash/exe3.1.rb
@@ -0,0 +1,26 @@
+=begin
+     
+     Faça um programa que leia 4 notas, mostre as notas e amedia na tela
+
+=end
+
+
+notas = []
+media = 0.0
+i = 0
+
+while i < 4
+
+    entrada = Float(gets())
+
+    notas << entrada
+
+    media = (notas.sum) / 4
+
+    i += 1
+
+end
+puts "-------------------"
+puts notas
+puts "-------------------"
+puts "A media é: #{media}"

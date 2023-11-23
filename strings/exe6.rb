5a7ddd0a76218af046109a5be1c5ff5e39308780
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-11-23 11:25:44 -0300

    exe6

---
+++ strings/exe6.rb
@@ -0,0 +1,11 @@
+print "Digte a primeira sting: "
+str1 = gets
+print "Digite a segunds string: "
+str2 = gets
+
+if str1 == str2
+    puts "Strings iguais"
+else  
+    puts "String diferentes"
+
+end

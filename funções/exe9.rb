d5e82115d153396d0d27678b652d7e29f806723d
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-11-29 11:25:50 -0300

    exe9

---
+++ funções/exe9.rb
@@ -0,0 +1,19 @@
+
+def soma(numero)
+
+    if numero > 0
+
+         array_digitos = numero.digits.reverse
+         soma = array_digitos.sum
+ 
+    elsif numero < 0
+
+        "Numero Inválido"
+
+    end
+
+end
+
+puts "A soma dos numeros é: #{soma(20)}"
+
+

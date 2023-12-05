f04f957baa8dc859432c7caa3003ce18e9603185
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-12-05 09:52:03 -0300

    poo

---
+++ poo/aula.rb
@@ -0,0 +1,26 @@
+class Carro 
+
+    def initialize(nome)
+
+        @nome = nome 
+
+    end
+
+    def nome=(value) # metodo set
+        @nome = value
+    end
+    
+    def nome  # metodo get
+        @nome
+    end
+
+    def mostrar(marca="Marca Padrão")
+        puts "Marca: #{marca} - Modelo: #{self.nome}" # equivale ao this
+    end
+
+end
+
+carro = Carro.new("fusca")
+#carro.nome = "Palio"
+
+puts carro.mostrar("Fiat")

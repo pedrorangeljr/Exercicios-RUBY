28910a5b73022220f8d9470ad64951d7f3a92a9d
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-12-27 11:03:40 -0300

    modlulo2

--- modulo/aula.rb
+++ modulo/aula.rb
@@ -4,25 +4,45 @@ def a.teste
     puts "1"
 end 
 
-class << a  
-    def teste2 
-        puts "2"
+
+class Pedro 
+    class << self 
+        attr_accessor :nome, :endereco, :telefone 
     end
-
-    attr_accessor :nome, :telefone, :idade 
-
-end 
-
-puts a.teste2
-
-puts a.nome = "Pedro"
-puts a.telefone = "123456789"
-puts a.idade = "37"
-
-module Teste 
-
-    class Carro 
-    end 
 end
 
-Teste::Carro.new
+Pedro.nome
+
+#module Utilidades 
+ #   class Cpf 
+ #       def validar  
+ #       end
+  #  end
+ #   class Cnpj 
+ #   end
+#end
+
+#Utilidades::Cpf
+
+module Utilidades 
+    def validar_cpf 
+        true 
+    end 
+    def validar_cnpj 
+        false
+    end
+end
+
+class Cliente 
+   include Utilidades 
+end
+
+puts Cliente.new.validar_cpf
+puts Cliente.new.validar_cnpj 
+
+b = "sss"
+b.extend Utilidades 
+
+puts b 
+puts b.validar_cpf
+puts b.validar_cnpj

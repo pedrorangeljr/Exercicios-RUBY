7f6af0d8d7e0538e5c0f94311d0855c4e990293d
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-01-04 10:56:21 -0300

    dinamicos3

--- dinamicos/aula.rb
+++ dinamicos/aula.rb
@@ -45,3 +45,41 @@ class Teste
   
   teste = Teste.new
   
+   ########################################################
+   module AtributosDinamicos
+    def atributos(*atrs)
+      atrs.each do |atr|
+        define_method("#{atr}=") do |value|
+          instance_variable_set "@#{atr}", value
+        end
+  
+        define_method("#{atr}") do
+          instance_variable_get "@#{atr}"
+        end
+      end
+    end
+  
+    def atributos_somente_leitura(*atrs)
+      atrs.each do |atr|
+        define_method("#{atr}") do
+          instance_variable_get "@#{atr}"
+        end
+      end
+    end
+  end
+  
+  class Teste
+    extend AtributosDinamicos
+    atributos :nome, :telefone, :cpf
+    # atributos_somente_leitura :nome, :telefone, :cpf
+    # attr_accessor :nome, :telefone
+  end
+  
+  t = Teste.new
+  # debugger
+  t.nome = "danilo"
+  t.telefone = "3334343"
+  t.cpf = "111.111.111-22"
+  
+  
+  puts "#{t.nome}, #{t.telefone}, #{t.cpf}"
